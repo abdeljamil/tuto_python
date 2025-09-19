fcb7dc80ac8af6dc7c377d3ba3e1351893696c32
abdeljamil <abdeljamil610@gmail.com>
2025-09-20 00:34:07 +0100

    update a file diference 2X et 3x

---
+++ différences_2x_3x.py
@@ -0,0 +1,99 @@
+import sys
+
+maVariable = 14 
+
+x = 186
+y = 223
+
+#python 2.x
+
+#print "bonjour"
+#print maVariable
+#print "maVariable =",maVariable
+#print "Sans retour à la ligne",
+#print
+#print ":".join([X,Y])
+#print >> sys.stderr, "message....."
+#donne = input("Entez un nombre :")
+#chaine = raw_input("Entez du text : ")
+
+# x = 123.0
+# y =14.6
+
+# if x <> y :
+
+
+# long 
+# long()
+
+
+
+# class Maclasse:
+#     __metaclass__ = MaMetaClasse
+
+#raise "message d'erreur"
+#raise TypeError ,"message"
+
+
+
+
+
+
+
+#python 3.X
+
+# print("bonjour")
+# print(maVariable)
+# print("bonjour",maVariable)
+# print("Sans retour à la ligne",end="")
+# print()
+# print(x,y,sep=":")
+# print("message.....",file=sys.stderr)
+# print("bonjour")
+# print(maVariable)
+# print("bonjour",maVariable)
+# print("Sans retour à la ligne",end="")
+# print()
+# print(x,y,sep=":")
+# print("message.....",file=sys.stdout)
+# donnee = input("Entez quelque chose : ")
+# donnee = int(donnee)
+# chaine = "C'était l'été, il faissait chaud"
+
+# x = 123.0
+# y =14.6
+
+# print(x/y)
+# print(x//y)
+
+
+# x = 123.0
+# y =14.6
+
+# if x != y:
+
+
+# int 
+# int()
+
+
+# class MaClasse(metaclass = MaMetaClasse):
+    
+#raise Exception("message d'erreur")
+
+#raise TypeError("message")
+
+
+
+#python 2.X       python 3.X
+# xrange()     ->    range()
+# long()       ->    int()
+# basesstring()->    str()
+
+# urlparse     ->  urllib.parse
+
+# cPickle -> pickle
+# Tkinter -> tkinter
+# thread -> _thread 
+# Queue -> queue
+# repr -> reprlib
