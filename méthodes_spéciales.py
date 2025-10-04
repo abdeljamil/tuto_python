3023756c0475edd56c2f392cc0523196188b2e0a
abdeljamil <113044449+abdeljamil@users.noreply.github.com>
2025-10-04 16:53:20 +0100

    update a file

--- méthodes_spéciales.py
+++ méthodes_spéciales.py
@@ -34,23 +34,119 @@
 
 
 
-class Lapin:
-    def __init__(self,name,weigth):
+# class Lapin:
+#     def __init__(self,name,weigth):
+#         self.name = name
+#         self.weigth = weigth
+
+#     def hello(self):
+#         print("hello !:")
+    
+#     def __repr__(self):
+#         return "Lapin {} de {}kg.".format(self.name,self.weigth)
+
+
+# lp = Lapin("coco",25)
+# lp2 = Lapin ("Titi",7)
+
+# if lp == lp2:
+#     print("Ce sont les mêmes")
+
+# else:
+#     print("Ces lapins sont différents")
+
+
+
+
+
+
+# class Lapin:
+#     def __init__(self,name,weigth):
+#         self.name = name
+#         self.weigth = weigth
+
+#     def hello(self):
+#         print("hello !:")
+    
+
+
+# lp = Lapin("coco",25)
+# lp2 = Lapin ("Titi",7)
+
+# if lp == lp2:
+#     print("Ce sont les mêmes")
+
+# else:
+#     print("Ces lapins sont différents")
+
+
+
+
+# class Lapin:
+#     def __init__(self,name,weigth)
+#         self.name = name
+#         self.weigth = weigth
+
+#     def hello(self):
+#         print("Hello ! :)")
+
+#     def __eq__(self,other):
+#         if self.weigth == other.weigth:
+#             return True
+#         return False
+
+# lp = Lapin("Coco",25)
+# lp2 = Lapin("Titi",7)
+
+# if lp = lp2:
+#     print("Ce sont les mêmes")
+# else:
+#     print("Ces lapins sont différents")
+
+
+
+
+
+
+# class Lapin:
+#     def __init__(self,name,weigth)
+#         self.name = name
+#         self.weigth = weigth
+
+#     def hello(self):
+#         print("Hello ! :)")
+
+#     def __eq__(self,other):
+#         if self.weigth == other.weigth:
+#             return True
+#         return False
+
+# lp = Lapin("Coco",7)
+# lp2 = Lapin("Titi",7)
+
+# if lp = lp2:
+#     print("Ce sont les mêmes")
+# else:
+#     print("Ces lapins sont différents")
+
+
+
+
+class Lapin: 
+    def __init__(self,name,weigth)
         self.name = name
         self.weigth = weigth
 
     def hello(self):
-        print("hello !:")
-    
-    def __repr__(self):
-        return "Lapin {} de {}kg.".format(self.name,self.weigth)
+        print("Hello ! :)")
 
+    def __eq__(self,other):
+        return self.weigth == other.weigth
 
-lp = Lapin("coco",25)
-lp2 = Lapin ("Titi",7)
+lp = Lapin("Coco",7)
+lp2 = Lapin("Titi",7)
 
 if lp == lp2:
-    print("Ce sont les mêmes")
-
+    print("ce sont les mêmes")
 else:
-    print("Ces lapins sont différents")
+    print("ces lapins sont differents")
