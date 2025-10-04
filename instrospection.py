168f8a9ed88a76593cc46cb5c677870a35965d92
abdeljamil <abdeljamil610@gmail.com>
2025-10-04 04:02:20 +0100

    update

--- instrospection.py
+++ instrospection.py
@@ -1,6 +1,6 @@
 class Item:
     """
-    Classe définissait un objet utilisable en jeu
+    Classe définissant un objet utilisable en jeu
     """
     def __init__(self,name,category,description,costValue = 1):
         self.name = name 
@@ -12,21 +12,67 @@ class Item:
         print("------------------------")
         print("[{} - {}]\n> {}\nPrix d'achat : {} po".format(self.name,self.category,self.description,self.costValue))
         print("-------------------------")
+        print(__name__)
 
 
 #code Principal
 
-it1 = Item("Epée en mousse","Arme","Magnifique épée au tranchant inexistant")
-it1.toString()
+
+age = 24 
+
+print(age.__doc__)
+
+# it1 = Item("Epée en mousse","Arme","Magnifique épée au tranchant inexistant")
+
+# print(it1.__doc__)
+
+# print(it1.name)
+# it1.name = "Arc"
+# print(it1.name)
 
 
 
+# print(it1.name)
+# it1.__dict__["name "]= "Arc"
+# print(it1.name)
+
+
+# print(it1.description)    c'est ne pas du tout trop conséille 
+# print(it1.__dict__["description"])
+
+#it2 = Item("Arc en bois de hêtre","Arme","Magnifique arc des gends archers")
+#it1.toString()
+
+#it1.toString()
+
+# print(id(it1))
+# print(id(it2))
 
 
 
+#print(dir(it1))
+
+# if __name__ == "__main__":
+#     print("Je suis dans la partie principale")
+
+
+# age = 24 
+
+# print(type(age))
+
+"""
+hasttr(), | getattr() | callable() | isinstance() 
+
+"""
+
+#help(isinstance)
+
+
+# print(isinstance(it1,Item))
 
 
 
+#print(it1.__dict__)
 
 
 
