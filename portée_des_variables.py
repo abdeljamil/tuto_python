c8ba838923933940e8aa7d09d53f71481c0ab246
abdeljamil <abdeljamil610@gmail.com>
2025-10-05 07:00:26 +0100

    update

---
+++ portée_des_variables.py
@@ -0,0 +1,97 @@
+# name = "jason"
+
+# print(name)
+
+
+
+
+
+# name = "jason"
+
+# def show_name():
+#     print(name)
+
+# show_name()
+
+
+
+
+
+
+
+# name = "jason"
+
+# def show_name():
+#     name = "Luc"
+#     print(name)
+
+# print(name)
+# show_name()
+
+
+
+
+
+
+
+
+
+
+# name = "jason"
+
+# def modify_name():
+#     name = "Luc"
+
+# print(name)
+# modify_name()
+# print(name)
+
+
+
+
+
+
+
+
+# name = "jason"
+
+# def modify_name():
+#     name = "Luc"
+#     return name
+
+
+# print(name)
+# name = modify_name()
+# print(name)
+
+
+
+
+
+
+
+# name = "jason"
+
+
+# def show_name(prenom):
+#     print(prenom)
+
+# show_name(name)
+
+
+
+
+
+
+
+
+
+name = "jason"
+
+def edit_name():
+    global name
+    name = "luc"
+
+print(name)
+edit_name()
+print(name)
